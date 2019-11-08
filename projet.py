7489a863637ac6b0d4b7fc417b17556043b0e2a1
jvittone <40358640+jvittone@users.noreply.github.com>
2019-11-08 11:10:09 +0100

    Update projet.py

--- projet.py
+++ projet.py
@@ -11,12 +11,21 @@ resultat = open("resultat.txt","w")
 i=0
 
 temp = []
+temp2 = []
 
 for ligne in sources:
 	temp += [ligne.split()]
+
+for ligne in secteurs:
+	temp2 += [ligne]
 	
-	
-print(temp[1])
+print (len(temp))
+print (len(temp2))
+
+for i in range(0,len(temp)):
+	for j in temp[i]:
+		if j == -1:
+			
 
 #Fermeture des fichiers
 
