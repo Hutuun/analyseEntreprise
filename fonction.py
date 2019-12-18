00eb506f50bd6356dc3a0f3507fb46d5db92e1a5
jvittone <40358640+jvittone@users.noreply.github.com>
2019-12-18 08:26:53 +0100

    Maj

--- fonction.py
+++ fonction.py
@@ -8,23 +8,103 @@ import matplotlib.pyplot as plt
 import pandas
 import fonction as fct
 
-def AffichageClassement(p,eigval):
+def FonctionPrincipale(sources2,secteurs2,caracteristique,nbEle,dimx,adresse):
+	sc = StandardScaler()
+	acp = PCA(svd_solver='full')
+
+	#Calcul des valeurs propres
+
+	Z = sc.fit_transform(sources2)
+	#print(Z)
+
+	n = len(sources2)
+	p = len(sources2[0])
+
+	coord = acp.fit_transform(Z)
+
+	eigval = (acp.singular_values_**2)/n
+
+	print(acp.explained_variance_ratio_)
+
+	#Determination du nombre de facteurs a retenir
+
+	bs = 1/numpy.arange(p,0,-1) 
+	bs = numpy.cumsum(bs) 
+	bs = bs[::-1]
+
+	print(pandas.DataFrame({'Val.Propre':eigval,'Seuils':bs,'Choisi':eigval>bs})) 
+
+	#Contribution à l'inertie
+	di = numpy.sum(Z**2,axis=1) 
+	print(pandas.DataFrame({'ID':secteurs2,'d_i':di})) 
+
+	#Qualité de représentation
+	cos2 = coord**2 
+	for j in range(p):     
+		cos2[:,j] = cos2[:,j]/di 
+ 
+	print(pandas.DataFrame({'id':secteurs2,'COS2_1':cos2[:,0],'COS2_2':cos2[:,1]}))
+
+	#Contribution à chaque axes
+	ctr = coord**2
+	for j in range(p):
+		ctr[:,j] = ctr[:,j]/(n*eigval[j])      
+ 
+	print(pandas.DataFrame({'id':secteurs2,'CTR_1':ctr[:,0],'CTR_2':ctr[:,1]})) 
+
+	#Représentation des variables
+	sqrt_eigval = numpy.sqrt(eigval) 
+
+	corvar = numpy.zeros((p,p))
+ 
+	for k in range(p):     
+		corvar[:,k] = acp.components_[k,:] * sqrt_eigval[k]
+
+	#print(corvar)
+
+	print(pandas.DataFrame({'id':caracteristique,'COR_1':corvar[:,0],'COR_2':corvar[:,1]}))
+
+	#Qualité de représentation des variables
+
+	cos2var = corvar**2 
+	print(pandas.DataFrame({'id':caracteristique,'COS2_1':cos2var[:,0],'COS2_2':cos2var[:,1]}))
+
+	#Contribution des variables aux axes
+
+	ctrvar = cos2var 
+ 
+	for k in range(p):     
+		ctrvar[:,k] = ctrvar[:,k]/eigval[k] 
+ 
+	print(pandas.DataFrame({'id':caracteristique,'CTR_1':ctrvar[:,0],'CTR_2':ctrvar[:,1]})) 
+
+	#Affichage
+
+	fct.AffichageClassement(p,eigval,adresse)
+
+	fct.AffichageCumul(p,acp,adresse)
+
+	fct.RepresentationIndividus(n,nbEle,dimx,coord,secteurs2,adresse)
+
+	fct.AffichageCercleCorrelation(caracteristique,corvar,p,adresse)
+
+def AffichageClassement(p,eigval,adresse):
 	plt.plot(numpy.arange(1,p+1),eigval)
 	plt.title("Classement en fonction de leur importance des valeurs propres")
 	plt.ylabel("Valeurs propres")
 	plt.xlabel("Classement")
-	plt.savefig("Image/fig1.png")
+	plt.savefig(adresse+"fig1.png")
 	plt.show()
 
-def AffichageCumul(p,acp):
+def AffichageCumul(p,acp,adresse):
 	plt.plot(numpy.arange(1,p+1),numpy.cumsum(acp.explained_variance_ratio_)) 
 	plt.title("Cumul de la représentativité des valeurs propres") 
 	plt.ylabel("Somme de la représentativité des valeurs propres") 
 	plt.xlabel("Nombres de valeurs propres") 
-	plt.savefig("Image/fig2.png")
+	plt.savefig(adresse+"fig2.png")
 	plt.show() 
 
-def RepresentationIndividus(n,nbEle,dimx,coord,secteurs2):
+def RepresentationIndividus(n,nbEle,dimx,coord,secteurs2,adresse):
 	for j in range(int(n/nbEle)):
 
 		#positionnement des individus dans le premier plan 
@@ -41,7 +121,7 @@ def RepresentationIndividus(n,nbEle,dimx,coord,secteurs2):
 		plt.plot([0,0],[-dimx,dimx],color='silver',linestyle='-',linewidth=1)
 
 		#affichage 
-		name = "Image/Correlation/fig" + str(j) + ".png"
+		name = adresse+"Correlation/fig" + str(j) + ".png"
 		fig.savefig(name)
 		fig.clear()
 		#plt.show()
@@ -49,7 +129,7 @@ def RepresentationIndividus(n,nbEle,dimx,coord,secteurs2):
 		plt.clf()
 		plt.close('all')
 		
-def AffichageCercleCorrelation(caracteristique,corvar,p):
+def AffichageCercleCorrelation(caracteristique,corvar,p,adresse):
 	fig, axes = plt.subplots(figsize=(8,8)) 
 	axes.set_xlim(-1,1) 
 	axes.set_ylim(-1,1) 
@@ -67,5 +147,5 @@ def AffichageCercleCorrelation(caracteristique,corvar,p):
 	axes.add_artist(cercle) 
  
 	#affichage 
-	plt.savefig("Image/CercleCorrelation.png")
+	plt.savefig(adresse+"CercleCorrelation.png")
 	plt.show() 
--- projet.py
+++ projet.py
@@ -28,14 +28,15 @@ secteurs = []
 sources = []
 secteurs2 = []
 sources2 = []
+secteurs3 = []
+sources3 = []
+sources4 = []
 
 caracteristique =[]
+caracteristique2 =[]
 
 Z = []
 coord = []
-
-sc = StandardScaler()
-acp = PCA(svd_solver='full')
 
 #Preparation des donnees
 
@@ -73,81 +74,30 @@ for i in range(0,len(secteurs)):
 del (secteurs2[0])
 del (sources2[0])
 
-#Calcul des valeurs propres
+fct.FonctionPrincipale(sources2,secteurs2,caracteristique,nbEle,dimx,"Image/")
 
-Z = sc.fit_transform(sources2)
-#print(Z)
+caracteristique2 +=[caracteristique[2]]
+caracteristique2 +=[caracteristique[3]]
 
-n = len(sources2)
-p = len(sources2[0])
+for i in range(0,len(sources2)):
+	temp = []
+	temp +=[sources2[i][2]]
+	temp +=[sources2[i][3]]
+	sources3+=[temp]
 
-coord = acp.fit_transform(Z)
+#print(pandas.DataFrame({'Secteur':secteurs2,'Sources':sources3})) 
 
-eigval = (acp.singular_values_**2)/n
+fct.FonctionPrincipale(sources3,secteurs2,caracteristique2,nbEle,dimx,"Image2/")
 
-print(acp.explained_variance_ratio_)
+for i in range(0,len(sources2)):
+	temp = []
+	temp +=[sources2[i][12]]
+	temp +=[sources2[i][23]]
+	sources4+=[temp]
 
-#Determination du nombre de facteurs a retenir
+#print(pandas.DataFrame({'Secteur':secteurs2,'Sources':sources3})) 
 
-bs = 1/numpy.arange(p,0,-1) 
-bs = numpy.cumsum(bs) 
-bs = bs[::-1]
-
-print(pandas.DataFrame({'Val.Propre':eigval,'Seuils':bs,'Choisi':eigval>bs})) 
-
-#Contribution à l'inertie
-di = numpy.sum(Z**2,axis=1) 
-print(pandas.DataFrame({'ID':secteurs2,'d_i':di})) 
-
-#Qualité de représentation
-cos2 = coord**2 
-for j in range(p):     
-	cos2[:,j] = cos2[:,j]/di 
- 
-print(pandas.DataFrame({'id':secteurs2,'COS2_1':cos2[:,0],'COS2_2':cos2[:,1]}))
-
-#Contribution à chaque axes
-ctr = coord**2
-for j in range(p):
-	ctr[:,j] = ctr[:,j]/(n*eigval[j])      
- 
-print(pandas.DataFrame({'id':secteurs2,'CTR_1':ctr[:,0],'CTR_2':ctr[:,1]})) 
-
-#Représentation des variables
-sqrt_eigval = numpy.sqrt(eigval) 
-
-corvar = numpy.zeros((p,p))
- 
-for k in range(p):     
-	corvar[:,k] = acp.components_[k,:] * sqrt_eigval[k]
-
-#print(corvar)
-
-print(pandas.DataFrame({'id':caracteristique,'COR_1':corvar[:,0],'COR_2':corvar[:,1]}))
-
-#Qualité de représentation des variables
-
-cos2var = corvar**2 
-print(pandas.DataFrame({'id':caracteristique,'COS2_1':cos2var[:,0],'COS2_2':cos2var[:,1]}))
-
-#Contribution des variables aux axes
-
-ctrvar = cos2var 
- 
-for k in range(p):     
-	ctrvar[:,k] = ctrvar[:,k]/eigval[k] 
- 
-print(pandas.DataFrame({'id':caracteristique,'CTR_1':ctrvar[:,0],'CTR_2':ctrvar[:,1]})) 
-
-#Affichage
-
-#fct.AffichageClassement(p,eigval)
-
-#fct.AffichageCumul(p,acp)
-
-#fct.RepresentationIndividus(n,nbEle,dimx,coord,secteurs2)
-
-#fct.AffichageCercleCorrelation(caracteristique,corvar,p)
+fct.FonctionPrincipale(sources4,secteurs2,caracteristique2,nbEle,dimx,"Image2/")
 
 #Fermeture des fichiers
 
